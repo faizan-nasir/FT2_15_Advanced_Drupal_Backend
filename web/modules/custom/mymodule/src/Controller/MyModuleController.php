3361d4e8c4cb7af390e334b500e557ca48547c30
SK Faizan Nasir <faizan.nasir@innoraft.com>
2024-07-19 18:20:29 +0530

    FT2-18: Added permissions.

---
+++ web/modules/custom/mymodule/src/Controller/MyModuleController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\mymodule\Controller;
+
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Controller\ControllerBase;
+
+class MyModuleController extends ControllerBase
+{
+  protected $currentUser;
+  public function __construct(AccountInterface $currentUser)
+  {
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * Returns the content for MyModule settings page.
+   *
+   * @return array
+   *   A render array containing the page content.
+   */
+  public function firstFunction()
+  {
+    $build = [
+      '#markup' => $this->t('This is the content of MyModule settings page.'),
+    ];
+    return $build;
+  }
+
+  /**
+   * Returns the content for MyModule settings page.
+   *
+   * @return array
+   *   A render array containing the page content.
+   */
+  public function checkPermission()
+  {
+    if ($this->currentUser->hasPermission('custom permission')) {
+      // Get specific user information.
+      $name = $this->currentUser->getDisplayName();
+      $uid = $this->currentUser->id();
+      $build = [
+        '#markup' => $this->t("<h3>Hello {$name} your id is: {$uid}</h3>"),
+      ];
+    }
+    else {
+      $build = [
+        '#markup' => $this->t("<h3>Permission Denied!</h3>"),
+      ];
+    }
+    return $build;
+  }
+}
